f03453ae67d2c456c85f97b27701393c0d9fe8a1
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-29 13:32:01 +0530

    create model list

---
+++ model/OrderDetailModel.js
@@ -0,0 +1,8 @@
+ class OrderDetailModel{
+    constructor(order_id, item_id, qty, unit_price) {
+        this.order_id = order_id;
+        this.item_id = item_id;
+        this.qty = qty;
+        this.unit_price = unit_price;
+    }
+}
---
+++ model/OrderModel.js
@@ -0,0 +1,7 @@
+ class OrderModel{
+    constructor(orderId, date, customerId) {
+        this.orderId = orderId;
+        this.date = date;
+        this.customerId = customerId;
+    }
+}
