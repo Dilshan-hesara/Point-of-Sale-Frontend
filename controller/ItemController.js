cd71fec00e9d8969fc8971bb48b04c01ee327af5
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-05-09 05:21:49 +0530

    Item add  up

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,2 +1,37 @@
-import { item_db } from "../db/db.js";
+import {customer_db, item_db} from "../db/db.js";
 import ItemModel from "../model/ItemModel.js";
+
+
+$("#saveAddItemBtn").click(function () {
+    let item_code = $("#item_code").val();
+    let item_description = $("#itemDescription").val();
+    let item_qty = $("#itemQty").val();
+    let item_price = $("#itemPrice").val();
+
+    if (item_code === '' || item_description === '' || item_qty === '' || item_price === '') {
+        Swal.fire("Error", "Please fill all fields", "error");
+    } else {
+        const item_data = new ItemModel(item_code, item_description, item_qty, item_price);
+        item_db.push(item_data);
+        loadCustomerTableData();
+
+        $('#ItemModal').modal('hide');
+        $('#ItemModal form')[0].reset();
+
+        Swal.fire("Success", "Item Added Successfully", "success");
+    }
+});
+
+
+function loadCustomerTableData() {
+    $('#item-tbody').empty();
+    item_db.forEach((item, index) => {
+        let row = `<tr>
+            <td>${item.item_code}</td>
+            <td>${item.item_description}</td>
+            <td>${item.item_qty}</td>
+            <td>${item.item_price}</td>
+        </tr>`;
+        $('#item-tbody').append(row);
+    });
+}
